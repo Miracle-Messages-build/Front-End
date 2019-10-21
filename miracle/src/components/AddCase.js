6cc15da1946c4e79e9a7042242f4dfd6d3b1c2de
Michael Nunes <miketnunes@gmail.com>
2019-10-21 14:30:54 -0700

    Edit case form, add basic style

--- miracle/src/components/AddCase.js
+++ miracle/src/components/AddCase.js
@@ -1,11 +1,49 @@
 import React, { useState } from 'react'
+import styled from 'styled-components';
+
+const FormContainer = styled.div`
+  margin-top: 5%; /* NOT FOR PRODUCTION */
+  width: 20%;
+  box-shadow: 0 0 2px 1px rgb(100, 100, 100, 0.5);
+`
+const FormHeader = styled.h1`
+  font-size: 1.8rem;
+  padding: 2%;
+  margin: 0;
+  text-align: center;
+  color: white;
+  background-color: dodgerblue;
+`
+
+const Form = styled.form`
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+  padding: 2%;
+  
+
+  label {
+    width: 100%;
+    margin-top: 1%;
+  }
+
+  input{
+    width: 100%;
+    padding: 2%;
+    margin: 3% auto;
+    box-sizing: border-box;
+  }
+  button {
+    width: 100%;
+    height: 30px;
+  }
+`
 
 const AddCase = () => {
   const [inputs, setInputs] = useState({
     name: '',
     age: '',
-    city: '',
-    state: '',
+    location: '',
     contact: ''
   });
 
@@ -19,14 +57,28 @@ const AddCase = () => {
   }
 
   return (
-    <form onSubmit={submitForm}>
-      <input type="text" name="name" id="name" value={inputs.name} onChange={handleChange} placeholder="Enter a name..." />
-      <input type="number" name="age" id="age" value={inputs.age} onChange={handleChange} min="16" max="100" placeholder="16" />
-      <input type="text" name="city" id="city" value={inputs.city} onChange={handleChange} placeholder="Los Angeles" />
-      <input type="text" name="state" id="state" value={inputs.state} onChange={handleChange} placeholder="CA" />
-      <input type="text" name="contact" id="contact" value={inputs.contact} onChange={handleChange} placeholder="Phone # or Address" />
-      <button type="submit">Create Post</button>
-    </form>
+    <FormContainer>
+      <FormHeader>Add Lost Family Member</FormHeader>
+      <Form onSubmit={submitForm}>
+        <label htmlFor="name">
+          Name
+          <input type="text" name="name" id="name" value={inputs.name} onChange={handleChange} placeholder="Name" required />
+        </label>
+        <label htmlFor="age">
+          Age
+          <input type="number" name="age" id="age" value={inputs.age} onChange={handleChange} min="16" max="100" placeholder="Age" required />
+        </label>
+        <label htmlFor="city">
+          Last Known Location
+          <input type="text" name="location" id="location" value={inputs.location} onChange={handleChange} placeholder="City, State" required />
+        </label>
+        <label htmlFor="contact">
+          Contact Info
+          <input type="text" name="contact" id="contact" value={inputs.contact} onChange={handleChange} placeholder="Phone # or Address" required />
+        </label>
+        <button type="submit">Create Post</button>
+      </Form>
+    </FormContainer>
   )
 }
 
