379d2f80ca201302b22eb3103e35ba0fd55c282b
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-21 13:29:26 -0400

    Added sign up form

---
+++ miracle/src/components/SignIn.js
@@ -0,0 +1,72 @@
+import React, {useState} from "react";
+import axios from 'axios'
+
+const SignUp = (props) => {
+
+    // {
+    //     "password": "string",
+    //     "primaryemail": "string",
+    //     "username": "string"
+    //   }
+
+    const credentials = {
+        username:'',
+        primaryemail:'',
+        password:''
+    }
+
+const [signUp, setSignUp] = useState(credentials)
+
+const handleChange = event => {
+  setSignUp ({
+    ...signUp,
+    [event.target.name]:event.target.value
+  });
+
+}
+
+const handleLogin = event => {
+  event.preventDefault();
+
+ axios
+      .post('', signUp)
+       .then(response => {
+          localStorage.setItem('token', response.data.payload);
+          props.history.push('/')
+      })
+      .catch(err => console.log(err.response));
+
+
+
+}
+
+  return (
+    <>
+          <form onSubmit={handleLogin}>
+                    <input
+                        type="text"
+                        name="username"
+                        value={signUp.username}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        value={signUp.password}
+                        onChange={handleChange}
+                    />
+                      <input
+                        type="primaryemail"
+                        name="password"
+                        value={signUp.primaryemail}
+                        onChange={handleChange}
+                    />
+                    <button>Log in</button>
+
+
+                </form>
+    </>
+  );
+};
+
+export default SignUp;
