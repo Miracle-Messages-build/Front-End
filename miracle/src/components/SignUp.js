e575d333a84994cc2ef2a332c04b8666c24d1b9a
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-22 16:53:56 -0400

    Login working

--- miracle/src/components/SignUp.js
+++ miracle/src/components/SignUp.js
@@ -1,73 +1,77 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import axios from 'axios'
 
 const SignUp = (props) => {
 
-    // {
-    //     "password": "string",
-    //     "primaryemail": "string",
-    //     "username": "string"
-    //   }
+  // {
+  //     "password": "string",
+  //     "primaryemail": "string",
+  //     "username": "string"
+  //   }
 
-    const credentials = {
-        username:'',
-        primaryemail:'',
-        password:''
-    }
+  const credentials = {
+    username: '',
+    primaryemail: '',
+    password: ''
+  }
 
-const [signUp, setSignUp] = useState(credentials)
+  const [signUp, setSignUp] = useState(credentials)
 
-const handleChange = event => {
-  setSignUp ({
-    ...signUp,
-    [event.target.name]:event.target.value
-  });
+  const handleChange = event => {
+    event.persist()
+    setSignUp({
+      ...signUp,
+      [event.target.name]: event.target.value
+    });
 
-}
+  }
 
-const handleLogin = event => {
-  event.preventDefault();
+  const handleLogin = event => {
+    event.preventDefault();
 
- axios
-      .post('', signUp)
-       .then(response => {
-          localStorage.setItem('token', response.data.payload);
-          props.history.push('/')
-      })
-      .catch(err => console.log(err.response));
+    axios
+      .post('https://lindseyacason-miraclemessages.herokuapp.com/createnewuser', signUp)
+      .then(response => console.log(response.data, "sign up"))
+
+    //  .then(response => {
+    //     localStorage.setItem('token', response.data.payload);
+        props.history.push('/login')
+    // })
+    .catch(err => console.log(err.response));
 
 
 
-}
+  }
 
   return (
     <>
-          <form className="signup-form" onSubmit={handleLogin}>
-                    <input 
-                        type="text"
-                        name="username"
-                        value={signUp.username}
-                        onChange={handleChange}
-                        placeholder="Username"
-                    />
-                    <input
-                        type="password"
-                        name="password"
-                        value={signUp.password}
-                        onChange={handleChange}
-                        placeholder="Password"
-                    />
-                      <input
-                        type="text"
-                        name="primaryemail"
-                        value={signUp.primaryemail}
-                        onChange={handleChange}
-                        placeholder="Primary Email"
-                    />
-                    <button>Sign up</button>
+      <form className="signup-form" onSubmit={handleLogin}>
+        <h1>Sign up</h1>
+        <input
+          type="text"
+          name="username"
+          value={signUp.username}
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <input
+          type="password"
+          name="password"
+          value={signUp.password}
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        <input
+          type="text"
+          name="primaryemail"
+          value={signUp.primaryemail}
+          onChange={handleChange}
+          placeholder="Primary Email"
+        />
+        <button>Sign up</button>
 
 
-                </form>
+      </form>
     </>
   );
 };
--- miracle/src/utils/axiosWithAuth.js
+++ miracle/src/utils/axiosWithAuth.js
@@ -1,12 +1,28 @@
 import axios from 'axios';
 
 export const axiosWithAuth = () => {
-  const token = localStorage.getItem('token');
- 
-  return axios.create({
-    baseURL: '',
-    headers: {
-      Authorization: token
-    }
-  });
+    const token = localStorage.getItem('token');
+
+    return axios.create({
+        // baseURL: 'https://lindseyacason-miraclemessages.herokuapp.com',
+        headers: {
+            Authorization: `Bearer ${token}`,
+         
+        }
+    });
 };
+
+
+export const axiosWithAuthLogin = () => {
+    
+    return axios.create({
+
+        headers: {
+            Authorization: `Basic ${btoa('lambda-client:lambda-secret')}`,
+            // Authorization: "Basic bGFtYmRhLWNsaWVudDpsYW1iZGEtc2VjcmV0",
+         
+            //   'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        // baseURL: 'https://lindseyacason-miraclemessages.herokuapp.com',
+    });
+}
