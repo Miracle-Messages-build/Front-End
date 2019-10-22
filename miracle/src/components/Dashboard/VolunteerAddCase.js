2e94c1845b2cb71d5e0199690d3ee5798110067e
Michael Nunes <miketnunes@gmail.com>
2019-10-22 13:59:57 -0700

    Sync form property names to endpoint property names

--- miracle/src/components/Dashboard/VolunteerAddCase.js
+++ miracle/src/components/Dashboard/VolunteerAddCase.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 const FormContainer = styled.div`
   margin-top: 2%; 
   width: 50%;
   box-shadow: 0 0 2px 1px rgb(100, 100, 100, 0.5);
-`
+`;
+
 const FormHeader = styled.h1`
   font-size: 1.8rem;
   padding: 2%;
@@ -13,7 +14,7 @@ const FormHeader = styled.h1`
   text-align: center;
   color: white;
   background-color: dodgerblue;
-`
+`;
 
 const Form = styled.form`
   display: flex;
@@ -42,11 +43,11 @@ const Form = styled.form`
     height: 40px;
     margin: 2% auto;
   }
-`
+`;
 
 const FormMain = styled.div`
   display: flex;
-`
+`;
 
 const FormSection = styled.div`
   width: 50%;
@@ -56,21 +57,24 @@ const FormSection = styled.div`
   :first-child{
     border-right: 1px solid black;
   }
-`
+`;
 
 const VolunteerAddCase = () => {
   const [inputs, setInputs] = useState({
-    name: '',
-    age: '',
-    homeTown: '',
-    currentCity: '',
-    contact: '',
+    socialCaseFname: '',
+    socialCaseLname: '',
+    socialCaseAge: '',
+    socialCaseHometown: '',
+    socialCaseCurrentTown: '',
+    socialCaseContactInfo: '',
+    socialCaseIsSensitive: false,
 
-    familyName: '',
-    familyAge: '',
-    relationship: '',
-    lastKnownLoc: '',
-    extraDetails: ''
+    socialCaseFamilyFName: '',
+    socialCaseFamilyLName: '',
+    socialCaseFamilyAge: '',
+    socialCaseFamilyRelationship: '',
+    socialCaseFamilyLastKnownLocation: '',
+    socialCaseNotes: ''
   });
 
   const handleChange = e => {
@@ -84,52 +88,64 @@ const VolunteerAddCase = () => {
 
   return (
     <FormContainer>
-      <FormHeader>Add New Missing Person Case</FormHeader>
+      <FormHeader></FormHeader>
       <Form onSubmit={submitForm}>
         <FormMain>
 
           <FormSection>
-            <label htmlFor="name">
-              Homeless Name
-          <input type="text" name="name" id="name" value={inputs.name} onChange={handleChange} placeholder="Name" required />
+            <label htmlFor="socialCaseFname">
+              Homeless Person's Name
+            <input type="text" name="socialCaseFname" id="socialCaseFname" value={inputs.socialCaseFname} onChange={handleChange} placeholder="First Name" required />
             </label>
-            <label htmlFor="age">
+            <label htmlFor="socialCaseLname">
+              Last Name
+            <input type="text" name="socialCaseLname" id="socialCaseLname" value={inputs.socialCaseLname} onChange={handleChange} placeholder="Last Name" required />
+            </label>
+            <label htmlFor="socialCaseAge">
               Age
-          <input type="number" name="age" id="age" value={inputs.age} onChange={handleChange} min="16" max="100" placeholder="Age" required />
+            <input type="text" name="socialCaseAge" id="socialCaseAge" value={inputs.socialCaseAge} onChange={handleChange} placeholder="Age" required />
             </label>
-            <label htmlFor="homeTown">
+            <label htmlFor="socialCaseHometown">
               Home Town
-          <input type="text" name="homeTown" id="homeTown" value={inputs.homeTown} onChange={handleChange} placeholder="City, State" required />
+            <input type="text" name="socialCaseHometown" id="socialCaseHometown" value={inputs.socialCaseHometown} onChange={handleChange} placeholder="City" required />
             </label>
-            <label htmlFor="currentCity">
+            <label htmlFor="socialCaseCurrentTown">
               Current City
-          <input type="text" name="currentCity" id="currentCity" value={inputs.currentCity} onChange={handleChange} placeholder="City" required />
+            <input type="text" name="socialCaseCurrentTown" id="socialCaseCurrentTown" value={inputs.socialCaseCurrentTown} onChange={handleChange} placeholder="City" required />
             </label>
-            <label htmlFor="contact">
+            <label htmlFor="socialCaseContactInfo">
               Contact Info
-          <input type="text" name="contact" id="contact" value={inputs.contact} onChange={handleChange} placeholder="Phone # or Address" required />
+            <input type="text" name="socialCaseContactInfo" id="socialCaseContactInfo" value={inputs.socialCaseContactInfo} onChange={handleChange} placeholder="Phone # or Address" required />
+            </label>
+            <label htmlFor="socialCaseIsSensitive">
+              Is Case Sensitive?
+              <input type="checkbox" name="socialCaseIsSensitive" id="socialCaseIsSensitive" checked={inputs.socialCaseIsSensitive} onChange={handleChange}/>
             </label>
           </FormSection>
           <FormSection>
-            <label htmlFor="familyName">
-              Family Member Name
-          <input type="text" name="familyName" id="familyName" value={inputs.familyName} onChange={handleChange} placeholder="Name" required />
+            <label htmlFor="socialCaseFamilyFName">
+              Friend or Family Member's Name
+            <input type="text" name="socialCaseFamilyFName" id="socialCaseFamilyFName" value={inputs.socialCaseFamilyFName} onChange={handleChange} placeholder="First Name" required />
             </label>
-            <label htmlFor="familyAge">
+            <label htmlFor="socialCaseFamilyLName">
+              Last Name
+            <input type="text" name="socialCaseFamilyLName" id="socialCaseFamilyLName" value={inputs.socialCaseFamilyLName} onChange={handleChange} placeholder="Last Name" required />
+            </label>
+            <label htmlFor="socialCaseFamilyAge">
               Age
-          <input type="text" name="familyAge" id="familyAge" value={inputs.familyAge} onChange={handleChange} placeholder="Age" required />
+            <input type="text" name="socialCaseFamilyAge" id="socialCaseFamilyAge" value={inputs.socialCaseFamilyAge} onChange={handleChange} placeholder="Age" required />
             </label>
-            <label htmlFor="relationship">
+            <label htmlFor="socialCaseFamilyRelationship">
               Relationship
-          <input type="text" name="relationship" id="relationship" value={inputs.relationship} onChange={handleChange} placeholder="Relative" required />
+            <input type="text" name="socialCaseFamilyRelationship" id="socialCaseFamilyRelationship" value={inputs.socialCaseFamilyRelationship} onChange={handleChange} placeholder="Relative" required />
             </label>
-            <label htmlFor="lastKnownLoc">
+            <label htmlFor="socialCaseFamilyLastKnownLocation">
               Last Known Location
-          <input type="text" name="lastKnownLoc" id="lastKnownLoc" value={inputs.lastKnownLoc} onChange={handleChange} placeholder="City, State" required />
+            <input type="text" name="socialCaseFamilyLastKnownLocation" id="socialCaseFamilyLastKnownLocation" value={inputs.socialCaseFamilyLastKnownLocation} onChange={handleChange} placeholder="City" required />
             </label>
-            <label htmlFor="extraDetails">
+            <label htmlFor="socialCaseNotes">
               Extra Details
-          <textarea name="extraDetails" id="extraDetails" value={inputs.extraDetails} onChange={handleChange} placeholder="Other family members, friends, last known job, etc" required />
+          <textarea name="socialCaseNotes" id="socialCaseNotes" value={inputs.socialCaseNotes} onChange={handleChange} placeholder="Other family members, friends, last known job, etc" required />
             </label>
           </FormSection>
         </FormMain>
