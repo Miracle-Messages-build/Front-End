18c22a09a14c0f87189c90620f38414b9ee8ae56
Michael Nunes <miketnunes@gmail.com>
2019-10-22 16:57:20 -0700

    Add initial public case buildout

--- miracle/src/components/PublicCases/AddPublicCase.js
+++ miracle/src/components/PublicCases/AddPublicCase.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 import styled from 'styled-components';
 
 const FormContainer = styled.div`
@@ -33,6 +34,13 @@ const Form = styled.form`
     margin: 3% auto;
     box-sizing: border-box;
   }
+
+  textarea {
+    width: 100%;
+    padding: 2%;
+    margin: 3% auto;
+    box-sizing: border-box;
+  }
   
   button {
     width: 100%;
@@ -42,10 +50,21 @@ const Form = styled.form`
 
 const AddCase = () => {
   const [inputs, setInputs] = useState({
-    name: '',
-    age: '',
-    location: '',
-    contact: ''
+    socialCaseFname: 'Yeet',
+    socialCaseLname: 'Yeeterson',
+    socialCaseAge: '22',
+    socialCaseFamilyLastKnownLocation: 'Yeet Town',
+    socialCaseContactInfo: 'yeet@yeetmail.com',
+    socialCaseNotes: 'Likes to yeet',
+    socialCaseHometown: 'placeholder',
+    socialCaseCurrentTown: 'placeholder',
+    socialCaseIsSensitive: false,
+    socialCaseFamilyFName: 'placeholder',
+    socialCaseFamilyLName: 'placeholder',
+    socialCaseFamilyAge: 'placeholder',
+    socialCaseFamilyRelationship: 'placeholder',
+    socialCaseFamilyNotes: 'placeholder',
+    "user": null
   });
 
   const handleChange = e => {
@@ -54,28 +73,57 @@ const AddCase = () => {
 
   const submitForm = e => {
     e.preventDefault();
-    console.log(inputs);
+    const someObj = {
+      "socialCaseId": 88,
+      "socialCaseFname": "Lindsey",
+      "socialCaseLname": "Mac",
+      "socialCaseAge": "21",
+      "socialCaseHometown": "Hollywood",
+      "socialCaseCurrentTown": "New Orleans",
+      "socialCaseContactInfo": "mac@gmail.com",
+      "socialCaseNotes": "These are family add notes",
+      "socialCaseIsSensitive": false,
+      "socialCaseFamilyFName": null,
+      "socialCaseFamilyLName": null,
+      "socialCaseFamilyAge": null,
+      "socialCaseFamilyRelationship": null,
+      "socialCaseFamilyLastKnownLocation": null,
+      "socialCaseFamilyNotes": null,
+      "user": null
+    }
+    console.log('Sending POST:', someObj);
+    axios.post('https://lindseyacason-miraclemessages.herokuapp.com/socialCases/socialCases/add', someObj)
+      .then(response => console.log('POST Response:', response))
+      .catch(err => console.log('POST Error:', err));
   }
 
   return (
     <FormContainer>
       <FormHeader>Add Lost Family Member</FormHeader>
       <Form onSubmit={submitForm}>
-        <label htmlFor="name">
-          Name
-          <input type="text" name="name" id="name" value={inputs.name} onChange={handleChange} placeholder="Name" required />
+        <label htmlFor="socialCaseFname">
+          First Name
+          <input type="text" name="socialCaseFname" id="socialCaseFname" value={inputs.socialCaseFname} onChange={handleChange} placeholder="First Name" required />
         </label>
-        <label htmlFor="age">
+        <label htmlFor="socialCaseLname">
+          Last Name
+          <input type="text" name="socialCaseLname" id="socialCaseLname" value={inputs.socialCaseLname} onChange={handleChange} placeholder="Last Name" required />
+        </label>
+        <label htmlFor="socialCaseAge">
           Age
-          <input type="number" name="age" id="age" value={inputs.age} onChange={handleChange} min="16" max="100" placeholder="Age" required />
+          <input type="number" name="socialCaseAge" id="socialCaseAge" value={inputs.socialCaseAge} onChange={handleChange} min="16" max="100" placeholder="Age" required />
         </label>
-        <label htmlFor="location">
+        <label htmlFor="socialCaseFamilyLastKnownLocation">
           Last Known Location
-          <input type="text" name="location" id="location" value={inputs.location} onChange={handleChange} placeholder="City, State" required />
+          <input type="text" name="socialCaseFamilyLastKnownLocation" id="socialCaseFamilyLastKnownLocation" value={inputs.socialCaseFamilyLastKnownLocation} onChange={handleChange} placeholder="City, State" required />
         </label>
-        <label htmlFor="contact">
+        <label htmlFor="socialCaseContactInfo">
           Contact Info
-          <input type="text" name="contact" id="contact" value={inputs.contact} onChange={handleChange} placeholder="Phone # or Address" required />
+          <input type="text" name="socialCaseContactInfo" id="socialCaseContactInfo" value={inputs.socialCaseContactInfo} onChange={handleChange} placeholder="Phone # or Address" required />
+        </label>
+        <label htmlFor="socialCaseNotes">
+          Extra Details
+          <textarea name="socialCaseNotes" id="socialCaseNotes" value={inputs.socialCaseNotes} onChange={handleChange} placeholder="Other family members, friends, last known job, etc" required />
         </label>
         <button type="submit">Create Post</button>
       </Form>
--- miracle/src/components/PublicCases/PublicCases.js
+++ miracle/src/components/PublicCases/PublicCases.js
@@ -1,12 +1,36 @@
 import React from 'react'
+import { Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-import PublicCase from './PublicCase';
+import ViewPublicCases from './ViewPublicCases';
+import AddPublicCase from './AddPublicCase';
 
-const PublicCases = () => {
+const CaseHeader = styled.header`
+  width: 80%;
+  margin: 0 auto;
+  text-align: center;
+`;
+
+const CaseContainer = styled.div`
+width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`
+
+const PublicCases = props => {
   return (
-    <>
+    <div>
+      <CaseHeader>
+        <Link to="/publiccases"><button>View Public Cases</button></Link>
+        <Link to="/publiccases/addcase"><button>Add Missing Person</button></Link>
+      </CaseHeader>
 
-    </>
+      <CaseContainer>
+        {props.location.pathname === "/publiccases" ? <ViewPublicCases /> : null}
+        <Route path="/publiccases/addcase" component={AddPublicCase} />
+      </CaseContainer>
+    </div>
   )
 }
 
