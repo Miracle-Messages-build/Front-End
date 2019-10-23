a6af604250a9fc67d12f030480510c97404207f7
Michael Nunes <miketnunes@gmail.com>
2019-10-23 14:45:46 -0700

    Clean up app js

--- miracle/src/App.js
+++ miracle/src/App.js
@@ -1,27 +1,20 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AddPublicCase from './components/PublicCases/AddPublicCase.js'
-import PublicCases from './components/PublicCases/PublicCases'
-import UpdateForm from './components/EditCase.js'
+import { Route } from "react-router-dom";
+import { connect } from 'react-redux';
+import { fetchCase, editCase, addCase } from './actions/index.js'
 import axios from 'axios';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Dashboard from './components/Dashboard/Dashboard.js'
+
 import Login from './components/Login.js'
 import SignUp from './components/SignUp.js'
+import Dashboard from './components/Dashboard/Dashboard.js'
+import PublicCases from './components/PublicCases/PublicCases'
+
+import UpdateForm from './components/EditCase.js'
 import PrivateRoute from './components/PrivateRoute.js'
-import VolunteerCase from './components/Dashboard/VolunteerCase.js'
-import VolunteerCases from './components/TestForRedux.js'
-import AddCase from './components/PublicCases/AddPublicCase.js'
-import ViewCases from './components/Dashboard/ViewCases.js'
 
-import { fetchCase, editCase, addCase } from './actions/index.js'
-import { connect } from 'react-redux';
-import VolunteerAddCase from './components/Dashboard/VolunteerAddCase';
-import PublicCase from './components/PublicCases/PublicCase';
+import './App.css';
 
-
-function App(props) {
+function App() {
   const [caseInfo, setCaseInfo] = useState([])
 
   useEffect(() => {
@@ -34,22 +27,11 @@ function App(props) {
 
   return (
     <div className="App">
-      <h1>Miracle messages</h1>
-
       <Route path="/login" component={Login} />
       <Route path="/signup" component={SignUp} />
       <Route path="/public" component={PublicCases} />
 
-      {/* <PrivateRoute exact path="/dashboard" component={Dashboard} /> */}
-
-      {/* <PrivateRoute
-        exact path="/dashboard/add/case"
-        render={props => {
-          return <VolunteerAddCase {...props} addCase={props.addcase} />;
-        }} component={VolunteerAddCase}
-      /> */}
-
-      < PrivateRoute
+      <PrivateRoute
         path="/dashboard"
         render={props => {
           return <Dashboard {...props} />;
@@ -58,32 +40,11 @@ function App(props) {
       />
 
       <PrivateRoute
-        path="/volunteer/case"
-        render={props => {
-          return <VolunteerCase {...props} />;
-        }} component={VolunteerCase}
-      />
-
-      <PrivateRoute
-        path="/volunteer/cases"
-        render={props => {
-          return <VolunteerCases {...props} />;
-        }} component={VolunteerCases}
-      />
-
-      <Route
         path="/volunteer/edit/:id"
         render={props => {
           return <UpdateForm {...props} editCase={props.editCase} caseInfo={caseInfo} setCaseInfo={setCaseInfo} />;
         }}
       />
-
-      {/* <Route
-        path="/volunteer/edit/:id"
-        render={props => {
-          return <UpdateForm {...props} caseInfo={caseInfo} setCaseInfo={setCaseInfo} />;
-        }}
-      /> */}
     </div >
   );
 }
