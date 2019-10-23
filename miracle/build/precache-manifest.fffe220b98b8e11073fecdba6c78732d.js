f4647c9af57ce9f26a0e7144094292dceb4c6d5f
Michael Nunes <miketnunes@gmail.com>
2019-10-23 15:39:36 -0700

    Initial Build

---
+++ miracle/build/precache-manifest.fffe220b98b8e11073fecdba6c78732d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe6104ede8c0439f620da8a639ffeb56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f02fe7a7bb9404c1ee67",
+    "url": "/static/css/main.568a2fb1.chunk.css"
+  },
+  {
+    "revision": "306b5445397c15f4648e",
+    "url": "/static/js/2.2e53622a.chunk.js"
+  },
+  {
+    "revision": "f02fe7a7bb9404c1ee67",
+    "url": "/static/js/main.97f93e4d.chunk.js"
+  },
+  {
+    "revision": "51126c430772aa1794ce",
+    "url": "/static/js/runtime-main.313c58ea.js"
+  }
+]);
