72e6706495c4f3331974ab40d1a16eff3394bae7
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-24 13:45:36 -0400

    Added unit tests

--- miracle/src/App.test.js
+++ miracle/src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render } from '@testing-library/react'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+import PublicCase from './components/PublicCases/PublicCase.js'
+import Footer from './components/Footer.js'
+
+test('Miracle header found in AddPublicCase.js ', () => {
+  const { getByText } = render(<PublicCase />);
+  getByText(/missing/i)
+})
+
+test('Miracle header found in AddPublicCase.js ', () => {
+  const { getByText } = render(<Footer />);
+  getByText(/copyright/i)
+})
+
