b5654cd819e9667f7a18cf48a5d2b7ea24aaec87
Michael Nunes <miketnunes@gmail.com>
2019-10-24 15:01:10 -0700

    Fixes for editing cases

--- miracle/src/components/Dashboard/ViewCases.js
+++ miracle/src/components/Dashboard/ViewCases.js
@@ -39,26 +39,26 @@ const ViewCases = props => {
 
   return (
     <>
-          <CaseContainer>
+      <CaseContainer>
 
-      {socialCases ? socialCases.map((socialCase, idx) => (
-        <VolunteerCase
-          key={idx}
-          id={socialCase.socialCaseId}
-          name={`${socialCase.socialCaseFname} ${socialCase.socialCaseLname}`}
-          age={socialCase.socialCaseAge}
-          homeTown={socialCase.socialCaseHomeTown}
-          currentCity={socialCase.socialCaseCurrentTown}
-          contact={socialCase.socialCaseContactInfo}
-          familyName={`${socialCase.socialCaseFamilyFName} ${socialCase.socialCaseFamilyLName}`}
-          familyAge={socialCase.socialCaseFamilyAge}
-          relationship={socialCase.socialCaseFamilyRelationship}
-          lastKnownLoc={socialCase.socialCaseFamilyLastKnownLocation}
-          extraDetails={socialCase.socialCaseNotes}
+        {socialCases ? socialCases.map((socialCase, idx) => (
+          <VolunteerCase
+            key={idx}
+            id={socialCase.socialCaseId}
+            name={`${socialCase.socialCaseFname} ${socialCase.socialCaseLname}`}
+            age={socialCase.socialCaseAge}
+            homeTown={socialCase.socialCaseHomeTown}
+            currentCity={socialCase.socialCaseCurrentTown}
+            contact={socialCase.socialCaseContactInfo}
+            familyName={`${socialCase.socialCaseFamilyFName} ${socialCase.socialCaseFamilyLName}`}
+            familyAge={socialCase.socialCaseFamilyAge}
+            relationship={socialCase.socialCaseFamilyRelationship}
+            lastKnownLoc={socialCase.socialCaseFamilyLastKnownLocation}
+            extraDetails={socialCase.socialCaseNotes}
           // deleteCase={deleteCase}
-        />
-      )) : null}
-            </CaseContainer>
+          />
+        )) : null}
+      </CaseContainer>
 
     </>
   );
