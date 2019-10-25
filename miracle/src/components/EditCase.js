413eb1d258475d6a80b3e0250a77876f86e86598
Michael Nunes <miketnunes@gmail.com>
2019-10-24 22:28:43 -0700

    Add better form validation, custom error UI

--- miracle/src/components/EditCase.js
+++ miracle/src/components/EditCase.js
@@ -3,6 +3,9 @@ import React, { useState, useEffect } from 'react';
 import { axiosWithAuth } from '../utils/axiosWithAuth.js'
 import styled from 'styled-components';
 
+import ErrorModal from './ErrorModal';
+
+
 const FormContainer = styled.div`
   margin-top: 5%;
   width: 20%;
@@ -74,6 +77,9 @@ const UpdateForm = props => {
   console.log(props, 'props in update')
   const [info, setInfo] = useState(initialInfo);
 
+  const [error, setError] = useState({ isActive: false, msg: '' });
+
+  const notesMaxChars = 150;
 
   useEffect(() => {
     const caseToEdit = props.caseInfo.find(
@@ -83,6 +89,11 @@ const UpdateForm = props => {
   }, [props.caseInfo, props.match.params.id])
 
   const handleChange = event => {
+    if (event.target.name === 'socialCaseNotes') {
+      if (event.target.value.length > notesMaxChars) {
+        return;
+      }
+    }
     event.persist();
     setInfo({
       ...info,
@@ -94,13 +105,42 @@ const UpdateForm = props => {
   const handleLogin = event => {
     event.preventDefault();
 
+    if (info.socialCaseFname.length < 2 || info.socialCaseFamilyFName.length < 2) {
+      setError({ isActive: true, msg: 'First name cannot be less than two characters.' });
+      return;
+    }
+
+    if (info.socialCaseLname.length < 2 || info.socialCaseFamilyLName.length < 2) {
+      setError({ isActive: true, msg: 'Last name cannot be less than two characters.' });
+      return;
+    }
+
+    if (info.socialCaseHometown.length < 4) {
+      setError({ isActive: true, msg: 'Home town cannot be less than four characters.' });
+      return;
+    }
+    if (info.socialCaseCurrentTown.length < 4) {
+      setError({ isActive: true, msg: 'Current town must not be less than four characters.' });
+      return;
+    }
+
+    if (info.socialCaseContactInfo.length < 4) {
+      setError({ isActive: true, msg: 'Contact info cannot be less than four characters.' });
+      return;
+    }
+
+    if (info.socialCaseFamilyLastKnownLocation.length < 4) {
+      setError({ isActive: true, msg: 'Last known location cannot be less than four characters.' });
+      return;
+    }
+
     axiosWithAuth()
       .put(`https://lindseyacason-miraclemessages.herokuapp.com/socialCases/socialCase/${props.match.params.id}`, info)
       // .then(response => console.log(response))
       .then(response => {
         props.setCaseInfo([...props.caseInfo, response.data])
+        props.history.push('/dashboard')
         window.location.reload();
-        props.history.push('/dashboard/viewallcases')
       })
       .catch(err => console.log(err))
 
@@ -114,10 +154,11 @@ const UpdateForm = props => {
 
 
     <div className="edit-parent">
+      {error.isActive ? <ErrorModal error={error.msg} setError={setError} /> : null}
       <FormContainer>
         <FormHeader>Edit Form</FormHeader>
 
-        <Form className="signup-form" onSubmit={e => { props.history.push('/dashboard/viewallcases'); handleLogin(e) }}>
+        <Form className="signup-form" onSubmit={e => { handleLogin(e) }}>
 
           <input
             type="text"
